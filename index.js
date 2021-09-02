ef2ff2e21fefa84deb2ac31db0ba50a6b6df61cc
Teddy-Livingstone Ememandu <livistteddy@gmail.com>
2021-09-02 15:13:23 +0100

    Make menu links work

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const menuBar = document.querySelector('.menu-bars')
 const navMenu = document.querySelector('.nav-menu')
 const header = document.querySelector('header')
 const closeBtn = document.querySelector('.bx-x')
+const menuLinks = document.querySelectorAll('.menu-links')
 
 menuBar.addEventListener('click', () => {
   navMenu.style.display = 'block'
@@ -10,6 +11,12 @@ menuBar.addEventListener('click', () => {
 closeBtn.addEventListener('click', () => {
   navMenu.style.display = 'none'
 })
+
+for (let link of menuLinks) {
+  link.addEventListener('click', () => {
+    navMenu.style.display = 'none'
+  })
+}
 
 // const divBar = document.createElement('div')
 // navMenu.style.height = '100%';
